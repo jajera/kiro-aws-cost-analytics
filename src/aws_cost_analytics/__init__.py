64fe3b509b3bac055ef51a532729c31fd60dfc69
semantic-release <semantic-release>
2026-06-15 12:56:01 +0000

    chore(release): 1.0.0

--- src/aws_cost_analytics/__init__.py
+++ src/aws_cost_analytics/__init__.py
@@ -6,5 +6,5 @@ and shared tool functions for querying AWS Cost Explorer.
 
 from aws_cost_analytics.constants import MCP_SERVER_NAME, TOOL_NAMES
 
-__version__ = "0.1.0"
+__version__ = "1.0.0"
 __all__ = ["MCP_SERVER_NAME", "TOOL_NAMES", "__version__"]
